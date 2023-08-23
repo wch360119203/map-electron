1e14c4a54a06262c69bba916769ea8308fde089a
wuch <wuch@shuizijie.com>
2023-08-23 18:22:50 +0800

    fix:取消全局注册elem icon

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import { router } from '@/render'
 import '@/assets/style.scss'
 import { createPinia } from 'pinia'
 const app = createApp(App)
 app.use(ElementPlus)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
 app.use(router)
 app.use(createPinia())
 app.mount('#app')
