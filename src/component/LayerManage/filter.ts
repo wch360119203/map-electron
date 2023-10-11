24b3002d0f4f06869d7e7895b4b2807e553d133b
wuch <wuch@shuizijie.com>
2023-10-11 18:23:57 +0800

    chore

--- src/component/LayerManage/filter.ts
+++
@@ -1,8 +0,0 @@
-import { BaseLayer } from '@antv/l7'
-import { workParam } from '@/render/store'
-
-type filterParams = Parameters<BaseLayer['filter']>
-/**筛选运营商 */
-export function filterByOperator(name: RegExp): filterParams {
-  return ['wp', (field: workParam) => name.test(field.operator)]
-}
